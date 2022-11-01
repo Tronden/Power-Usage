1c90cfc602e4823da74bcee26484a12c364100c0
Tronden <Trond-zachariassen@outlook.com>
2022-11-01 21:55:36 +0100

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-files = os.listdir("Innføring ingeniørfag\Power Usage\Power-Usage\data")
-
-               
-selceted = int(input("Choose date: 1 = Nov 2020, 2 = Des 2020, 3 = Jan 2021: "))
-print(files[selceted-1])
